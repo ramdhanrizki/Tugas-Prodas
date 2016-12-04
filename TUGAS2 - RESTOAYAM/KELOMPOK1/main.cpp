414bf741cf84c1391c5c065de2624998342317f1
Ramdhan Rizki <Ramdhan Rizki>
2016-12-04 08:52:49 +0700

    Initial Commit

---
+++ TUGAS2 - RESTOAYAM/KELOMPOK1/main.cpp
@@ -0,0 +1,60 @@
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+const int MAX_ARRAY=100;
+int costumer = 0; 
+int stok_ayam = 50; 
+int stok_bebek =50; 
+
+int pesanan_ayam[MAX_ARRAY];
+int pesanan_bebek[MAX_ARRAY];
+
+void cetak_menu();
+int pilih_menu();
+
+void pemesanan(int *costumer,int pesanan_ayam[MAX_ARRAY],int pesanan_bebek[MAX_ARRAY])
+{
+          
+}
+int main(int argc, char *argv[])
+{
+    do{
+       cetak_menu();
+       pilih_menu();
+       switch(menu) {
+          case 1:
+               
+               break;                       
+          case 2:
+               
+               break;
+          case 3:
+                    
+       }
+    } while(menu!=4);    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
+
+void cetak_menu()
+{
+    system("cls"); 
+    cout<<"MENU RESTO AYAM"<<endl; 
+    cout<<"=============================="<<endl; 
+    cout<<"1. Pemesanan"<<endl; 
+    cout<<"2. Cetak Seluruh Pemesanan"<<endl;
+    cout<<"3. Stok Ayam dan Bebek"<<endl;
+    cout<<"4. Keluar"<<endl; 
+}
+
+int pilih_menu()
+{
+    int menu;
+    cout<<"Menu yang dipilih : "; 
+    cin>>menu;
+    return menu; 
+}

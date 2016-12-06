bc3b4074865ef44334cc2572a91630555ab671ce
Ramdhan Rizki <Ramdhan Rizki>
2016-12-06 21:12:05 +0700

    Update fungsi pemesanan dan cetak stok

--- TUGAS2 - RESTOAYAM/KELOMPOK1/main.cpp
+++ TUGAS2 - RESTOAYAM/KELOMPOK1/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -9,31 +10,92 @@ int costumer = 0;
 int stok_ayam = 50; 
 int stok_bebek =50; 
 
-int pesanan_ayam[MAX_ARRAY];
-int pesanan_bebek[MAX_ARRAY];
-
 void cetak_menu();
 int pilih_menu();
 int menu; 
+int pelanggan=0; 
+int jumlah_pesan; 
+int i; 
 
-void pemesanan(int *costumer,int pesanan_ayam[MAX_ARRAY],int pesanan_bebek[MAX_ARRAY])
+//record 
+typedef struct
 {
-          
+   int jumlah_ayam; 
+   int jumlah_bebek;
+}data_pesanan; 
+
+void gotoxy(int x, int y){
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+void pemesanan(int *pelanggan,int *jumlah_pesan,data_pesanan pesanan[MAX_ARRAY])
+{
+     system("cls");
+     *pelanggan+=1;     
+     bool input=false;
+     //gotoxy(5,9); cout<<"Selamat datang!!";
+     cout<<"Pelanggan no : "<<*pelanggan<<endl;
+     cout<<"Masukkan jumlah pemesanan : ";
+     cin>>*jumlah_pesan;
+     for(i=0;i<*jumlah_pesan;i++)
+     {
+        do{                                              
+            cout<<"Banyak ayam-"<<i+1<<" : "; 
+            cin>>pesanan[i].jumlah_ayam; 
+            if(stok_ayam<pesanan[i].jumlah_ayam) {
+               cout<<"Mohon maaf stok ayam hanya tinggal : "<<stok_ayam<<"..."<<endl;
+               getch();
+               system("cls");
+            } else {
+              cout<<"Banyak bebek-"<<i+1<<" : "; 
+              cin>>pesanan[i].jumlah_bebek;
+              if(stok_bebek<pesanan[i].jumlah_bebek){
+                 cout<<"Mohon maaf stok ayam hanya tinggal : "<<stok_ayam<<"..."<<endl;                                    
+                 getch();
+                 system("cls");
+              } else {
+                 input=true;    
+              }
+            }
+        }while(input==false); 
+        stok_ayam -= pesanan[i].jumlah_ayam;                 
+        stok_bebek -= pesanan[i].jumlah_bebek;
+     }  
+     cout<<"Pesanan anda telah disimpan tekan semabarang untuk kembali..";
+     getch();   
+}
+
+void cetak_stok()
+{
+    system("cls"); 
+    cout<<"STOK AYAM DAN BEBEK RESTO"<<endl; 
+    cout<<"=========================="<<endl; 
+    cout<<"STOK AYAM    : "<<stok_ayam<<endl; 
+    cout<<"STOK BEBEK   : "<<stok_bebek<<endl; 
+    cout<<"=========================="<<endl;
+    cout<<endl;
+    cout<<"Tekan sembarang untuk kembali ke menu..";
+    getch(); 
+}
+data_pesanan pesanan[MAX_ARRAY];
+
 int main(int argc, char *argv[])
 {
     do{
        cetak_menu();
-       pilih_menu();
+       menu = pilih_menu();
        switch(menu) {
           case 1:
-               
+               pemesanan(&pelanggan,&jumlah_pesan,pesanan);
                break;                       
           case 2:
                
                break;
           case 3:
-               
+               cetak_stok();
                break;       
        }
     } while(menu!=4);    

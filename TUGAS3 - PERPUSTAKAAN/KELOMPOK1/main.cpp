0c522ecdb82479414102a6e184ef28f646dc3970
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2016-12-12 09:05:58 +0700

    Perbaiki syntax dan spasi

--- TUGAS3 - PERPUSTAKAAN/KELOMPOK1/main.cpp
+++ TUGAS3 - PERPUSTAKAAN/KELOMPOK1/main.cpp
@@ -8,22 +8,22 @@ const int MAX_DATA = 100;
 const int DENDA = 10000; // denda bisa diubah terserah kesepakatan
 
 // Deklarasi struct
-typedef struct 
+typedef struct
 {
-   int tgl; 
-   int bln; 
-   int thn;             
-} tanggal; 
+   int tgl;
+   int bln;
+   int thn;
+} tanggal;
 
 typedef struct
 {
-   char kode[6]; 
-   char nama_peminjam[61]; 
+   char kode[6];
+   char nama_peminjam[61];
    char alamat_peminjam[81];
    char kode_buku[6];
    char judul_buku[81];
-   tanggal tgl_pinjam; 
-} dataPeminjaman; 
+   tanggal tgl_pinjam;
+} dataPeminjaman;
 
 typedef struct
 {
@@ -31,8 +31,8 @@ typedef struct
    char judul[81];
    char pengarang[61];
    char penerbit[61];
-   int stok;            
-} dataBuku; 
+   int stok;
+} dataBuku;
 
 typedef struct
 {
@@ -40,10 +40,10 @@ typedef struct
     char nama_peminjam[61];
     char kode_buku[6];
     char judul_buku[81];
-    tanggal tgl_kembali; 
-    int lama; 
-    int biaya_pinjam;   
-} dataPengembalian; 
+    tanggal tgl_kembali;
+    int lama;
+    int biaya_pinjam;
+} dataPengembalian;
 
 // variabel Global
 dataBuku            buku[MAX_DATA];
@@ -58,8 +58,8 @@ int                 ptrPengembalian;
 
 //Inisialisasi Procedure
 void cetak_menu();
-void menu_buku(); 
-void menu_peminjaman(); 
+void menu_buku();
+void menu_peminjaman();
 void menu_pengembalian();
 void cetak_buku();
 void cari_buku();
@@ -67,56 +67,57 @@ void tambah_buku();
 void edit_buku();
 void hapus_buku();
 
-//program utama 
+//program utama
 int main(int argc, char *argv[])
 {
-    int menu; 
+    int menu;
 
     do {
-        cetak_menu(); 
+        cetak_menu();
         cout<<"Menu yang dipilih  : ";
-        cin>>menu; 
+        cin>>menu;
 
         switch(menu)
         {
-            case 1: 
+            case 1:
                  menu_buku();
-                 break; 
+                 break;
             case 2:
                  menu_peminjaman();
-                 break; 
+                 break;
             case 3:
                  menu_pengembalian();
                  break;
             case 4:
-                 cout<<"Terimakasih telah menggunakan program kami.."; 
-                 getch();        
+                 cout<<"Terimakasih telah menggunakan program kami..";
+                 getch();
         }
-    } while(menu!=4); 
+    } while(menu!=4);
     return EXIT_SUCCESS;
 }
 
 void cetak_menu()
 {
-    system("cls"); 
-    cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl; 
-    cout<<"         UNIKOM BANDUNG      "<<endl;      
+    system("cls");
+    cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl;
+    cout<<"         UNIKOM BANDUNG      "<<endl;
     cout<<"============================="<<endl;
-    cout<<"1. Buku"<<endl; 
-    cout<<"2. Peminjaman"<<endl; 
-    cout<<"3. Pengembalian"<<endl; 
-    cout<<"4. Keluar"<<endl; 
+    cout<<"1. Buku"<<endl;
+    cout<<"2. Peminjaman"<<endl;
+    cout<<"3. Pengembalian"<<endl;
+    cout<<"4. Keluar"<<endl;
 }
+
 void cetak_buku()
 {
     int i;
 
     system("cls");
-    cout<<"CETAK BUKU";
-    cout<<"==========";
+    cout<<"CETAK BUKU"<<endl;
+    cout<<"=========="<<endl;
 
     for (i = 0; i <= ptrBuku; i++) {
-       cout<<"["<<i+1<<"]."<<endl; 
+       cout<<"["<<i+1<<"]."<<endl;
        cout<<"Judul     = "<<buku[i].judul<<endl;
        cout<<"Kode      = "<<buku[i].kode<<endl;
        cout<<"Penerbit  = "<<buku[i].penerbit<<endl;
@@ -125,9 +126,10 @@ void cetak_buku()
        cout<<endl;
     }
 
-    getch(); 
+    getch();
 }
 
+/* mengembalikan -1 bila data tidak ditemukan */
 int cariBukuByKode(int kode) {
     int i;
 
@@ -136,7 +138,7 @@ int cariBukuByKode(int kode) {
             break;
         }
     }
-    
+
     if (buku[i].kode == kode) {
         return i;
     } else {
@@ -155,7 +157,7 @@ void cari_buku()
     do {
         cout<<"Masukkan kode buku = ";
         cin>>kode_buku;
-        
+
         indeks_buku = cariBukuByKode(kode_buku);
         if (indeks_buku == -1) {
             cout<<"Buku yang diminta tidak ditemukan!"<<endl;
@@ -171,16 +173,17 @@ void cari_buku()
     cout<<"Pengarang : "<<buku[indeks_buku].pengarang<<endl;
     cout<<"Stok : "<<buku[indeks_buku].stok<<endl;
 
-    getch(); 
+    getch();
 }
+
 void tambah_buku()
 {
     int i;
     int jumlah_buku;
 
     system("cls");
-    cout<<"TAMBAH BUKU";
-    cout<<"===========";
+    cout<<"TAMBAH BUKU"<<endl;
+    cout<<"==========="<<endl;
 
     cout<<"Jumlah buku yang ingin ditambah = ";
     cin>>jumlah_buku;
@@ -201,8 +204,9 @@ void tambah_buku()
         cout<<endl;
     }
 
-    getch(); 
+    getch();
 }
+
 void edit_buku()
 {
     int kode_buku, indeks_buku;
@@ -214,13 +218,13 @@ void edit_buku()
     do {
         cout<<"Masukkan kode buku = ";
         cin>>kode_buku;
-        
+
         indeks_buku = cariBukuByKode(kode_buku);
         if (indeks_buku == -1) {
             cout<<"Buku yang diminta tidak ditemukan!"<<endl;
         }
     while (indeks_buku == -1);
-    
+
     cout<<endl;
     cout<<"Detail buku"<<endl;
     cout<<"==========="<<endl;
@@ -243,8 +247,9 @@ void edit_buku()
     cout<<"Stok = ";
     cin>>buku[indeks_buku].stok;
 
-    getch(); 
+    getch();
 }
+
 void hapus_buku()
 {
     int kode_buku, i;
@@ -256,7 +261,7 @@ void hapus_buku()
     do {
         cout<<"Masukkan kode buku = ";
         cin>>kode_buku;
-        
+
         indeks_buku = cariBukuByKode(kode_buku);
         if (indeks_buku == -1) {
             cout<<"Buku yang diminta tidak ditemukan!"<<endl;
@@ -270,11 +275,11 @@ void hapus_buku()
 
     // lakukan pengulangan sebanyak ptrBuku - indeks_buku
     for (i = 0; i < ptrBuku - indeks_buku) {
-        buku[indeks_buku + i].judul = buku[indeks_buku + 1+i].judul;  
-        buku[indeks_buku + i].kode = buku[indeks_buku + 1+i].kode;  
-        buku[indeks_buku + i].penerbit = buku[indeks_buku + 1+i].penerbit;  
-        buku[indeks_buku + i].pengarang = buku[indeks_buku + 1+i].pengarang;  
-        buku[indeks_buku + i].stok = buku[indeks_buku + 1+i].stok;  
+        buku[indeks_buku + i].judul = buku[indeks_buku + 1+i].judul;
+        buku[indeks_buku + i].kode = buku[indeks_buku + 1+i].kode;
+        buku[indeks_buku + i].penerbit = buku[indeks_buku + 1+i].penerbit;
+        buku[indeks_buku + i].pengarang = buku[indeks_buku + 1+i].pengarang;
+        buku[indeks_buku + i].stok = buku[indeks_buku + 1+i].stok;
     }
 
     // mengosongkan indeks terakhir
@@ -286,123 +291,123 @@ void hapus_buku()
 
     ptrBuku = ptrBuku - 1;
 
-    getch(); 
+    getch();
 }
 
 void menu_buku()
 {
-    int menu;  
+    int menu;
 
     do {
-        system("cls"); 
-        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl; 
-        cout<<"         UNIKOM BANDUNG      "<<endl;  
-        cout<<"     MENU BUKU PERPUSTAKAAN  "<<endl;    
+        system("cls");
+        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl;
+        cout<<"         UNIKOM BANDUNG      "<<endl;
+        cout<<"     MENU BUKU PERPUSTAKAAN  "<<endl;
         cout<<"============================="<<endl;
-        cout<<"1. Cetak Buku"<<endl; 
-        cout<<"2. Cari Buku"<<endl; 
-        cout<<"3. Tambah Buku"<<endl; 
-        cout<<"4. Edit Buku"<<endl;   
+        cout<<"1. Cetak Buku"<<endl;
+        cout<<"2. Cari Buku"<<endl;
+        cout<<"3. Tambah Buku"<<endl;
+        cout<<"4. Edit Buku"<<endl;
         cout<<"5. Hapus Buku"<<endl;
-        cout<<"6. Kembali"<<endl; 
-        cout<<"Menu yang dipilih : "; 
-        cin>>menu;         
+        cout<<"6. Kembali"<<endl;
+        cout<<"Menu yang dipilih : ";
+        cin>>menu;
 
         switch(menu) {
              case 1:
                   cetak_buku();
-                  break;           
+                  break;
              case 2:
                   cari_buku();
-                  break; 
+                  break;
              case 3:
                   tambah_buku();
-                  break; 
+                  break;
              case 4:
                   edit_buku();
-                  break; 
+                  break;
              case 5:
                   hapus_buku();
-                  break;     
-        }       
-    } while(menu!=6);    
+                  break;
+        }
+    } while(menu!=6);
 }
 
-/* 
+/*
    Bagian Peminjaman Buku
 */
 void cetak_peminjaman()
 {
    cout<<"PROCEDURE CETAK PEMINJAMAN  ";
-   getch();  
+   getch();
 }
 void peminjaman_buku()
 {
    cout<<"PROCEDURE PEMINJAMAN BUKU  ";
-   getch();  
+   getch();
 }
 void menu_peminjaman()
 {
-   
-    int menu;  
+
+    int menu;
     do{
         system("cls");
-        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl; 
-        cout<<"         UNIKOM BANDUNG      "<<endl;  
-        cout<<"      MENU PEMINJAMAN BUKU   "<<endl;    
+        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl;
+        cout<<"         UNIKOM BANDUNG      "<<endl;
+        cout<<"      MENU PEMINJAMAN BUKU   "<<endl;
         cout<<"============================="<<endl;
-        cout<<"1. Cetak Data Peminjaman"<<endl; 
-        cout<<"2. Peminjaman Buku"<<endl; 
-        cout<<"3. Kembali"<<endl; 
-        cout<<"Menu yang dipilih : "; 
-        cin>>menu;         
+        cout<<"1. Cetak Data Peminjaman"<<endl;
+        cout<<"2. Peminjaman Buku"<<endl;
+        cout<<"3. Kembali"<<endl;
+        cout<<"Menu yang dipilih : ";
+        cin>>menu;
         switch(menu){
              case 1:
                   cetak_peminjaman();
-                  break;           
+                  break;
              case 2:
                   peminjaman_buku();
-                  break;  
-        }       
-    } while(menu!=3);    
+                  break;
+        }
+    } while(menu!=3);
 }
 
 
-/* 
+/*
    Bagian Pengembalian Buku
 */
 void cetak_pengembalian()
 {
    cout<<"PROCEDURE CETAK PENGEMBALIAN  ";
-   getch();  
+   getch();
 }
 void pengembalian_buku()
 {
    cout<<"PROCEDURE PENGEMBALIAN BUKU  ";
-   getch();  
+   getch();
 }
 void menu_pengembalian()
 {
-     
-    int menu;  
+
+    int menu;
     do{
-        system("cls"); 
-        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl; 
-        cout<<"         UNIKOM BANDUNG      "<<endl;  
-        cout<<"    MENU PENGEMBALIAN BUKU   "<<endl;    
+        system("cls");
+        cout<<"SISTEM INFORMASI PERPUSTAKAAN"<<endl;
+        cout<<"         UNIKOM BANDUNG      "<<endl;
+        cout<<"    MENU PENGEMBALIAN BUKU   "<<endl;
         cout<<"============================="<<endl;
-        cout<<"1. Data Pengembalian"<<endl; 
-        cout<<"2. Pengembalian Buku"<<endl; 
-        cout<<"3. Kembali"<<endl; 
-        cout<<"Menu yang dipilih : "; 
-        cin>>menu;         
+        cout<<"1. Data Pengembalian"<<endl;
+        cout<<"2. Pengembalian Buku"<<endl;
+        cout<<"3. Kembali"<<endl;
+        cout<<"Menu yang dipilih : ";
+        cin>>menu;
         switch(menu){
              case 1:
                   cetak_pengembalian();
-                  break;           
+                  break;
              case 2:
                   pengembalian_buku();
-                  break;  
-        }       
-    } while(menu!=3);    
+                  break;
+        }
+    } while(menu!=3);
 }
